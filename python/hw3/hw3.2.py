d0ab64a969a7cc0b3b55a2588110f3be677c7eff
Ramil Mufazzalov <aoho92@gmail.com>
2022-12-09 18:49:17 +0500

    доделал

--- python/hw3/hw3.2.py
+++ python/hw3/hw3.2.py
@@ -1,1 +1,11 @@
 # Напишите программу, которая найдёт произведение пар чисел списка. Парой считаем первый и последний элемент, второй и предпоследний и т.д.
+
+listdef = list(range(-1, 6))
+print(listdef)
+i = 0
+
+while (i < (len(listdef) // 2)):
+    print(listdef[i] * listdef[(len(listdef) - i) - 1])
+    i += 1
+if (len(listdef) % 2 != 0):
+    print(listdef[len(listdef) // 2] ** 2)
--- python/hw3/hw3.3.py
+++ python/hw3/hw3.3.py
@@ -1,1 +1,11 @@
 # Задайте список из вещественных чисел. Напишите программу, которая найдёт разницу между максимальным и минимальным значением дробной части элементов.
+
+listdef = [1.34, 0.723, 4.5, 7.1238, 34.32451]
+print(listdef)
+listfract = []
+for i in listdef:
+    listfract.append(round((i - int(i)), 5))
+listfract.sort()
+listmin = listfract[0]
+listmax = listfract[-1]
+print(f'{listmax} - {listmin} = {listmax - listmin}')
